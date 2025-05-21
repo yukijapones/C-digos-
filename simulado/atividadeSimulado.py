1fa76f55af93421aa64afc43363956f3f3fb024e
yukijapones <viniciusyukiharu@outlook.com>
2025-05-21 00:25:57 -0300

    Add files via upload

---
+++ simulado/atividadeSimulado.py
@@ -0,0 +1,27 @@
+numero = int(input("Qual número deseja?: "))
+
+if numero % 2 == 0:
+    print("Seu número é par!")
+    multiPar = numero * 2
+    print("O resultado multiplicado por 2 é:", multiPar)
+else:
+    print("Seu número é ímpar!")
+    multiImpar = numero * 3
+    print("O resultado multiplicado por 3 é:", multiImpar)
+
+print()  # Linha em branco para separar
+
+if numero < 10:
+    conta = numero + 10 * 5 
+    print("O resultado da conta menor que 10 é:", conta)
+
+
+
+
+
+    
+
+    
+
+    
+
